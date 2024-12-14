b0e1b6b25ac6e22868855daadc8b8531a7409308
Miqayel dev <tech_dev@amlincubator.com>
2024-12-14 13:32:11 +0400

    add class

---
+++ database/migrations/2024_12_13_125911_create_settings_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->integer('slider_interval')->default(5000);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('settings');
+    }
+};
