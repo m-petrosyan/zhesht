4ca73037e6a3530a79326d6918b99d1eff0419b5
Miqayel <tech_dev@amlincubator.com>
2025-03-02 01:15:14 +0400

    fix css

--- resources/js/Components/Gallery/PastShows.vue
+++ resources/js/Components/Gallery/PastShows.vue
@@ -4,7 +4,6 @@ import "@splidejs/splide/dist/css/splide.min.css";
 import {AutoScroll} from "@splidejs/splide-extension-auto-scroll";
 import {formatDateTime} from "@/Helpers/dateFormatHelper.js";
 import {Link} from "@inertiajs/vue3";
-import NavLink from "@/Components/NavLink.vue";
 
 
 defineProps({
@@ -36,26 +35,27 @@ const splideOptions = {
 
 <template>
     <div class="flex bg-main justify-end">
-        <NavLink :href="route('past')">
+        <Link :href="route('past')">
             <h2 class="text-2xl text-black pr-2">Past events</h2>
-        </NavLink>
+        </Link>
     </div>
     <Splide :options="splideOptions" :extensions="{ AutoScroll }" aria-label="Past events">
         <SplideSlide v-for="item in pastTours" :key="item.id">
-            <img :src="item.poster_file?.thumb ?? item.banner_file.thumb" :alt="item.title"
-                 class="h-96 w-full">
-            <div class="info flex flex-col justify-end h-4/6 absolute bottom-0 w-full">
-                <div class="text-white text-center pb-10">
-                    <Link :href="route('event.show',item.id)"
-                          class="">
+            <Link :href="route('event.show',item.id)">
+                <img :src="item.poster_file?.thumb ?? item.banner_file.thumb" :alt="item.title"
+                     class="h-96 w-full">
+                <div class="info flex flex-col justify-end h-4/6 absolute bottom-0 w-full">
+                    <div class="text-white text-center pb-10">
+
                         <h3 class="text-3xl">{{ item.title }}</h3>
                         <div v-for="event in item.events" class="mt-5">
                             <b class="text-2xl">{{ formatDateTime(event.date_time, 'D MMMM YYYY ') }}</b>
                             <p class="text-2xl">{{ event.location }}</p>
                         </div>
-                    </Link>
+
+                    </div>
                 </div>
-            </div>
+            </Link>
         </SplideSlide>
     </Splide>
 </template>
