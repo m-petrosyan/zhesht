f92272a6ac5e1e3a70e654483cc42073395f8dcf
Miqayel <miqayelpetrosyan@gmail.com>
2025-03-20 13:02:16 +0400

    fix css

--- resources/js/Pages/Dashboard/Dashboard.vue
+++ resources/js/Pages/Dashboard/Dashboard.vue
@@ -8,7 +8,7 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
             <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
                 <div
                     class="overflow-hidden shadow-sm sm:rounded-lg dark:bg-gray-800">
-                    <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <div class="p-6 text-gray-100">
                         You're logged in!
                     </div>
                 </div>
