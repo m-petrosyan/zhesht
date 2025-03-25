a2ef3cfe0e6a11ee6ef1dcfd9b86c7340e736327
Miqayel <tech_dev@amlincubator.com>
2025-03-25 16:19:46 +0400

    add many things

--- app/Repositories/TourRepository.php
+++ app/Repositories/TourRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repositories;
 
+use App\Models\Event;
 use App\Models\Tour;
 use Illuminate\Database\Eloquent\Collection;
 
@@ -12,12 +13,20 @@ class TourRepository
         return Tour::query()
             ->with([
                 'events' => function ($query) {
-                    $query->orderBy('date_time', 'asc');
+                    $query->orderBy('date_time', 'asc'); // Загружаем ивенты, отсортированные по дате
                 },
             ])
             ->whereHas('events', function ($query) {
-                $query->where('date_time', '>', now());
+                $query->where('date_time', '>', now()); // Только туры с будущими ивентами
             })
+            ->select('tours.*') // Выбираем все поля из таблицы tours
+            ->addSelect([
+                'earliest_event_date' => Event::select('date_time')
+                    ->whereColumn('events.tour_id', 'tours.id')
+                    ->orderBy('date_time', 'asc') // Берем ближайшую дату
+                    ->limit(1)
+            ])
+            ->orderBy('earliest_event_date', 'asc') // Сортируем туры по ближайшей дате ивента
             ->get();
     }
 
